0cacb1f20331b7789ba94bf669adb837409989ed
lesteven <le_steven@outlook.com>
2019-06-04 15:15:56 -0700

    refactor: response code

--- internal/res/response.go
+++ internal/res/response.go
@@ -5,7 +5,21 @@ import (
     "encoding/json"
 )
 
-func SendSuccess(w http.ResponseWriter, data interface{}) {
+func SendJson(w http.ResponseWriter, status int, data interface{}) {
+    w.WriteHeader(status)
     w.Header().Add("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(data)
+}
+
+type ErrJson struct {
+    Err string `json:"error"`
+}
+
+func SendError(w http.ResponseWriter, status int, errMsg string) {
+    w.WriteHeader(status)
+    w.Header().Add("Content-Type", "application/json")
+    data := ErrJson {
+        Err: errMsg,
+    }
     json.NewEncoder(w).Encode(data)
 }
