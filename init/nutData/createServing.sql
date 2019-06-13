e84da6315b9fcd2f64579cd8232853d835489279
lesteven <le_steven@outlook.com>
2019-06-13 15:36:09 -0700

    chore: fix createServing sql typo

--- init/nutData/createServing.sql
+++ init/nutData/createServing.sql
@@ -4,8 +4,8 @@ CREATE TABLE serving (
     NDB_No integer REFERENCES product (NDB_No) ON DELETE CASCADE,
     Serving_Size VARCHAR(10),
     Serving_Size_UOM VARCHAR(4),
-    Househould_Serving VARCHAR(10),
-    Househould_Serving_UOM TEXT,
+    Household_Serving VARCHAR(10),
+    Household_Serving_UOM TEXT,
     Preparation_State VARCHAR(20)
 );
 
