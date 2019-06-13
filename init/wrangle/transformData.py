08e24e33fb2021464d2e57433384ed9f6bf4ebcc
lesteven <le_steven@outlook.com>
2019-06-13 13:15:06 -0700

    update transform script

--- init/wrangle/transformData.py
+++ init/wrangle/transformData.py
@@ -1,8 +1,12 @@
 import psycopg2
+from elasticsearch import Elasticsearch
 
 # postgresql db connection
 conn = psycopg2.connect("dbname=nutrition user=steven")
 cur = conn.cursor()
+
+# elasticsearch
+es = Elasticsearch()
 
 # sql statement template
 sql = "SELECT long_name, nutrient_name, output_value, output_uom, \
@@ -11,28 +15,46 @@ sql = "SELECT long_name, nutrient_name, output_value, output_uom, \
     INNER JOIN serving ON product.ndb_no = serving.ndb_no \
     WHERE product.ndb_no = %s"
 
+
+def writeErr(log):
+    with open(log, "a") as myfile:
+        myfile.write(prod_num + "\n")
+
 # create json from sql queries, then insert into elasticsearch
 def getData(prod_num):
-    num = "45127487"
-    prod_num = prod_num.replace('"','')
     cur.execute(sql,(prod_num,))
     data = cur.fetchall()
-    print(data[0][0])
-    for each in data:
-        print(each[1], each[2], each[3])
-    print()
+    if data and data[0] and data[0][0]:
+        json = {'Name': data[0][0]}
+        for each in data:
+            json[each[1]] = {'Value': each[2], 'Unit': each[3]}
+        return json
+    else:
+        writeErr("getErr.txt")
 
-foodData = '/home/steven/Downloads/nutData/data/Products.csv'
+
+def insertData(json, prod_num):
+    res = es.index(index='product', doc_type='food', id=prod_num, body=json)
+    if res['_shards']['failed'] != 0:
+        writeErr("inserErr.txt")
+
+
+# foodData = '/home/steven/Downloads/nutData/data/Products.csv'
+foodData = '/home/steven/Downloads/nutData/data/SomeProducts.csv'
 
 # access file and read first csv value in each line 
-i = 0
+# i = 0
 with open(foodData) as f:
     for line in f:
-        prod_num = line.split(',')[0]
-        getData(prod_num)
+        prod_num = line.split(',')[0].replace('"','')
+        json = getData(prod_num)
+        if json:
+            insertData(json, prod_num)
+        '''
         i += 1
         if i == 2:
             break
+            '''
 
 cur.close()
 conn.close()
