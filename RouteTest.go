0f533cc638fe8961833f989a66020d8b0e55284e
lesteven <le_steven@outlook.com>
2019-06-14 22:48:38 -0700

    WIP: get req.body

--- RouteTest.go
+++ RouteTest.go
@@ -3,12 +3,44 @@ package main
 import (
     "net/http"
     "database/sql"
+    "fmt"
+    "encoding/json"
+    //"io/ioutil"
 )
 
+type SearchData struct {
+    Search string `json:"search"`
+}
 
 func TestHandler(db *sql.DB) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        data := GetFood(db)
-        SendJson(w, http.StatusOK, data)
+        switch r.Method {
+            case http.MethodGet:
+                data := GetFood(db)
+                SendJson(w, http.StatusOK, data)
+            case http.MethodPost:
+                var search SearchData
+
+                err := json.NewDecoder(r.Body).Decode(&search)
+
+                if err != nil {
+                    fmt.Println(err)
+                    fmt.Println("error with decoder")
+                    panic(err)
+                }
+
+                fmt.Println(search.Search)
+                fmt.Fprintf(w, "hello post!") 
+
+                /*
+                fmt.Println(r.Body)
+                data, err := ioutil.ReadAll(r.Body)
+                if err != nil {
+                    panic(err)
+                }
+                fmt.Println(data)
+                fmt.Println(string(data))
+                */
+        }
     }
 }
