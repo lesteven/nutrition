7cd102553c50b47dd905ced30b59c37b741fd468
lesteven <le_steven@outlook.com>
2019-06-17 14:46:54 -0700

    chore: fix search scripts

--- init/wrangle/scripts/es/searchPage.sh
+++ init/wrangle/scripts/es/searchPage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl localhost:9200/product/_search?pretty \
+curl "localhost:9200/product/_search?size=2&from=6" \
     -H 'Content-Type: application/json' \
     -d '{"query" : 
             {
                "fuzzy": { 
-                    "name": "chicken"
+                    "name": "mochi"
                 }
             } 
         }'\
